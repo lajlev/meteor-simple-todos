366e137a4bad10058d39759de95d55074beaf36a
Michael Lajlev <mla@mobilethink.com>
2014-10-30 11:20:09 +0100

    step7Done

--- simple-todos.js
+++ simple-todos.js
@@ -1,9 +1,42 @@
+// Create DB collection
 Tasks = new Mongo.Collection("tasks")
 
+// If client side
 if (Meteor.isClient) {
+
+  // Fetch tasks from DB
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({})
+      return Tasks
+        .find( {}, {sort: {createdAt: -1} } )
     }
-  })
+  }) //// body.helpers
+
+  Template.body.events({
+    'submit .new-task': function (event) {
+      
+      // Fetch value
+      var text = event.target.text.value
+      
+      // Insert in DB
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      })
+      
+      // Clear and prevent default behavior
+      event.target.text.value = ""
+      return false
+    }
+  }) //// body.events
+
+  Template.task.events({
+    'click .toggle-checked': function () {
+      Tasks.update(this._id, {$set: {checked: ! this.checked} } )
+    },
+    'click .delete': function () {
+      Tasks.remove(this._id)
+    }
+  }) //// task.events
+
 }
